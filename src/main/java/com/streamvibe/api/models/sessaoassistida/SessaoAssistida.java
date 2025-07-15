4a19834ce49384aeee7c424d4d86d32cd5f017d9
Joao Guilherme <marmore.besta@gmail.com>
2025-07-14 21:42:02 -0300

    subindo sla

---
+++ src/main/java/com/streamvibe/api/models/sessaoassistida/SessaoAssistida.java
@@ -0,0 +1,8 @@
+package main.java.com.streamvibe.api.models.sessaoassistida;
+
+public class SessaoAssistida {
+    private Long id;
+    private int progressoEmSegundos;
+    private String dataUltimaVisualizacao;
+    private Boolean completo;
+}
